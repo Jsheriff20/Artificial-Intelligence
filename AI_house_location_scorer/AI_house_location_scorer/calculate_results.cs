d415945c09a9af087ceb5b743ea1acf61482f2a5
jack <jackasheriff@hotmail.co.uk>
2020-02-05 23:13:21 +0000

    started creating distance from location via car in KM however having problems with the json nests

--- AI_house_location_scorer/AI_house_location_scorer/calculate_results.cs
+++ AI_house_location_scorer/AI_house_location_scorer/calculate_results.cs
@@ -49,6 +49,41 @@ namespace AI_house_location_scorer
 
 
 
+        //returns distance between two points via roads in KM (requires long and lat of both start and end points) 
+        string get_car_distance_between_two_points(string[] start_point, string[] end_point)
+        {
+            string start_point_lat = start_point[0].ToString();
+            string start_point_long = start_point[1].ToString();
+            string end_point_lat = end_point[0].ToString();
+            string end_point_long = end_point[1].ToString();
+            string key = "c4KS0Km05Xs8ondgP5Zc~lkdQr73GTTjDT_MbverZjQ~AnONN35zZHkE8U9WpqwmS_ESLNYL-drcWCibvMfmynWAi0Urwbip4pYO0jDRi7wH";
+
+            string uri = "DistanceMatrix?origins=" + start_point_lat + "," + start_point_long + "&destinations=" + end_point_lat + "," + end_point_long + "&travelMode=driving&key=" + key;
+            //request api json data
+            var client = new RestClient("https://dev.virtualearth.net/REST/v1/Routes/");
+            var request = new RestRequest(uri, Method.GET);
+
+            //get response and decode it
+            var response = client.Execute(request);
+            JsonDeserializer deserialise = new JsonDeserializer();
+            var json_response = deserialise.Deserialize<Dictionary<string, string>>(response);
+
+
+            //encode result into json array then decode the inner array
+            IRestResponse rest_response = new RestResponse {Content = json_response["resourceSets"]};
+            var decoded_response = deserialise.Deserialize<Dictionary<string, string>>(rest_response); //the rest_response value ends up having content with square brackets (works fine when these first pair of square brackets are removed)
+
+
+
+            //return longitude & latitude array
+            string travel_distance = decoded_response["travelDistance"];
+            return travel_distance;
+        }
+
+
+
+
+
         //gets a list of recorded crimes in a area when given longitude and latitude
         List<String> get_recorded_crimes_in_area(string postcode)
         {
@@ -90,12 +125,15 @@ namespace AI_house_location_scorer
         private void calculate_results_Load(object sender, EventArgs e)
         {
 
-            List<String> crimes_list = get_recorded_crimes_in_area("ne38 9ex");
-            foreach (String crime in crimes_list)
-            {
-                Console.WriteLine(crime);
-            }
+            //List<String> crimes_list = get_recorded_crimes_in_area("ne38 9ex");
+            //foreach (String crime in crimes_list)
+            //{
+            //    Console.WriteLine(crime);
+            //}
 
+            string[] start_point = {"47.6044", "-122.3345"};
+            string[] end_point = {"45.5347", "-122.6231"};
+            Console.WriteLine(get_car_distance_between_two_points(start_point, end_point));
         }
     }
 }
